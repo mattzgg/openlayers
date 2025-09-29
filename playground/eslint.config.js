ae193977c28d7eb8ccb79a13b16433c96e88c2c2
Matthew Gong <matthew.zhigang.gong@gmail.com>
2025-09-29 22:42:57 +1300

    Add eslint

---
+++ playground/eslint.config.js
@@ -0,0 +1,36 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import vuePlugin from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+
+export default [
+  {
+    files: ["src/**/*.{ts,vue}"],
+    ignores: ["dist", "node_modules"],
+    languageOptions: {
+      parser: vueParser, // main parser for .vue files
+      parserOptions: {
+        parser: tsParser, // parser for <script setup lang="ts">
+        ecmaVersion: "latest",
+        sourceType: "module",
+        extraFileExtensions: [".vue"], // ensures TS parser sees .vue files
+      },
+    },
+    plugins: {
+      vue: vuePlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      // Vue rules
+      "vue/multi-word-component-names": "off",
+      "vue/no-multiple-template-root": "error",
+
+      // TypeScript rules
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
