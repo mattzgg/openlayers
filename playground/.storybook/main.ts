e17ff768942445af2f1056d7155439850c488711
Matthew Gong <matthew.zhigang.gong@gmail.com>
2025-09-29 23:49:05 +1300

    Make storybook recognize the path aliases

--- playground/.storybook/main.ts
+++ playground/.storybook/main.ts
@@ -1,19 +1,26 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import type { StorybookConfig } from "@storybook/vue3-vite";
+import path from "node:path";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  async viteFinal(config) {
+    config.resolve ??= {};
+    config.resolve.alias ??= {};
+    Object.assign(config.resolve.alias, {
+      "@": path.resolve(__dirname, "../src"),
+      "@ol": path.resolve(__dirname, "../../build/ol"),
+    });
+    return config;
+  },
 };
 export default config;
